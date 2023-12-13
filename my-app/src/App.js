07cb16a343fad8dd1a2b92fd37f61179a161164a
Bipin <bhabishwar.201744@ncit.edu.np>
2023-12-14 00:06:01 +0545

    AddedFunctions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 function App() {
+  const name = 'Josh Perez';
+  const isMale=false;
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {user}!</h1>
+    } return <h1>Hello, Stranger.</h1>
+
+  }
   return (
     <div className="App">
-      <h1>
-        Hello World!
-      </h1>
+      {getGreeting('')};
+      <h2>You are a {isMale? 'Male':'Female'}</h2>
     </div>
   );
 }
